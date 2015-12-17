d56dd5aa0e711ac1b33ecf3a1c5fa1e2455b7d86
Toomore Chiang <toomore0929@gmail.com>
2015-12-17 02:32:08 +0000

    Pretty.

--- cmd/lazyflickr_groupsinfo/main.go
+++ cmd/lazyflickr_groupsinfo/main.go
@@ -4,25 +4,29 @@ import (
 	"flag"
 	"log"
 	"os"
+	"runtime"
 	"sync"
 
+	"github.com/fatih/color"
 	"github.com/toomore/lazyflickrgo/flickr"
 )
 
 var (
 	apikey = flag.String("apikey", os.Getenv("FLICKRAPIKEY"), "Flickr API Key")
 	secret = flag.String("secret", os.Getenv("FLICKRSECRET"), "Flickr secret")
+	info   = color.New(color.Bold, color.FgGreen).SprintfFunc()
+	wg     sync.WaitGroup
 )
 
 func main() {
 	flag.Parse()
 	f := flickr.NewFlickr(*apikey, *secret)
-	var wg sync.WaitGroup
 	wg.Add(len(flag.Args()))
 	for _, val := range flag.Args() {
 		go func(val string) {
+			runtime.Gosched()
 			groupInfo := f.GroupsGetInfo("", val)
-			log.Printf("%s: %s", val, groupInfo.Group.ID)
+			log.Printf("%s => %s\n", val, info(groupInfo.Group.ID))
 			wg.Done()
 		}(val)
 	}
