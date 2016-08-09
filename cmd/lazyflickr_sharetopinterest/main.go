ba2bd2f1b36fe46fb3dc949e77cf28c12bcf8bad
Toomore Chiang <toomore0929@gmail.com>
2016-08-09 10:54:07 +0800

    Add `cmd/lazyflickr_sharetopinterest`.

---
+++ cmd/lazyflickr_sharetopinterest/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+// Pinterest type struct
+type Pinterest struct {
+	AccessToken string
+}
+
+// APIURL pinterest base.
+const APIURL = "https://api.pinterest.com"
+
+// Get HTTP GET
+func (p Pinterest) Get(path string, params url.Values) (*http.Response, error) {
+	if params == nil {
+		params = url.Values{}
+	}
+	params.Set("access_token", p.AccessToken)
+	url := fmt.Sprintf("%s%s?%s", APIURL, path, params.Encode())
+	log.Printf("Get %s\n", url)
+	return http.Get(url)
+}
+
+func (p Pinterest) me() {
+	resp, err := p.Get("/v1/me/", nil)
+	if err == nil {
+		body, _ := ioutil.ReadAll(resp.Body)
+		log.Printf("%s\n", body)
+	}
+}
+
+func main() {
+	pin := &Pinterest{AccessToken: os.Getenv("PINTEREST_TOKEN")}
+	pin.me()
+}
